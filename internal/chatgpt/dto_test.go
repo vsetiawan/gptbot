d89b65f3a09448c9a4ffbd1bcd125d9d9bf7a9c6
Vincent Setiawan <setiawan.vincent@rocketmail.com>
2023-04-22 00:06:38 +0800

    Fix dto_test.go

--- internal/chatgpt/dto_test.go
+++ internal/chatgpt/dto_test.go
@@ -1,40 +1,25 @@
 package chatgpt
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestChatCompletionResponse_firstChoiceMessageContent(t *testing.T) {
-	type fields struct {
-		ID      string
-		Object  string
-		Created int64
-		Model   string
-		Usage   struct {
-			PromptTokens     int `json:"prompt_tokens"`
-			CompletionTokens int `json:"completion_tokens"`
-			TotalTokens      int `json:"total_tokens"`
-		}
-		Choices []struct {
-			Message struct {
-				Role    string `json:"role"`
-				Content string `json:"content"`
-			} `json:"message"`
-			FinishReason string `json:"finish_reason"`
-			Index        int    `json:"index"`
-		}
-	}
 	tests := []struct {
 		name   string
-		fields *fields
+		fields *ChatCompletionResponse
 		want   string
 	}{
 		{
 			name: "",
-			fields: func() *fields {
-				f := &fields{}
-				f.Choices[0].Message.Content = "content"
+			fields: func() *ChatCompletionResponse {
+				f := &ChatCompletionResponse{}
+				actualContentValue := "content"
+				fString := fmt.Sprintf(`{"choices":[{"message":{"content": "%v"}}]}`, actualContentValue)
+				_ = json.Unmarshal([]byte(fString), f)
 				return f
 			}(),
 			want: "content",
@@ -42,14 +27,7 @@ func TestChatCompletionResponse_firstChoiceMessageContent(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			c := &ChatCompletionResponse{
-				ID:      tt.fields.ID,
-				Object:  tt.fields.Object,
-				Created: tt.fields.Created,
-				Model:   tt.fields.Model,
-				Usage:   tt.fields.Usage,
-				Choices: tt.fields.Choices,
-			}
+			c := tt.fields
 			assert.Equalf(t, tt.want, c.firstChoiceMessageContent(), "firstChoiceMessageContent()")
 		})
 	}
